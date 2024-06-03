9b6b3ccde62d23652f65e1d170d3b21d366e90f1
yaneric <2243398@cegepmontpetit.ca>
2024-06-03 10:42:18 -0400

    FCT ZombieType Index
    Ajout du contrôleur ZombieType et de la vue Index associée affichant la liste des types de zombies. Ajout du menu de gestion avec Bootstrap.

---
+++ Moi/ZombieParty/Controllers/ZombieTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+namespace ZombieParty.Controllers
+{
+    public class ZombieTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MaListe = new List<ZombieType>()
+            {
+                new ZombieType(){TypeName= "Virus", Id=1},
+                new ZombieType(){TypeName= "Contact", Id=2}
+            };
+            return View();
+
+        }
+    }
+}
